34a3f603e25ed1781f354497f8c3b80ee2743c5d
duyanhan1995 <duyanhan1995@gmail.com>
2018-05-11 09:00:46 +0800

    用户信息实体类

--- src/main/java/com/duyanhan/forum/domain/UserInfo.java
+++ src/main/java/com/duyanhan/forum/domain/UserInfo.java
@@ -1,5 +1,12 @@
 package com.duyanhan.forum.domain;
 
+import java.util.List;
+
+/**
+ * 用户信息实体类
+ * @author duyanhan
+ *
+ */
 public class UserInfo {
 
 	private Integer id;
@@ -7,35 +14,8 @@ public class UserInfo {
 	private String password;
 	private String sex;
 	private String personalDescription;
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	public String getPersonalDescription() {
-		return personalDescription;
-	}
-	public void setPersonalDescription(String personalDescription) {
-		this.personalDescription = personalDescription;
-	}
-	
+	// 一个用户可以发表多个帖子
+	private List<Post> posts;
+	// 一个用户可以发表多个回复
+	private List<Comment> comments;
 }
