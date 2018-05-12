dc9dbeacf27af9667663804cd3b98b79a11e1232
duyanhan1995 <duyanhan1995@gmail.com>
2018-05-13 00:30:34 +0800

    出现问题：WEB-INF目录下的JSP文件中引用的WEB-INF目录外的样式文件不生效

--- src/main/java/com/duyanhan/forum/controller/HomeController.java
+++ src/main/java/com/duyanhan/forum/controller/HomeController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value="/index")
+	@RequestMapping(value="/")
 	public String home() {
-		return "index";
+		return "home";
 	}
 }
