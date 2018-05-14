69a0992ccdb0a879ac7b44859354dfac55fe56e8
duyanhan1995 <duyanhan1995@gmail.com>
2018-05-14 10:16:33 +0800

    公共控制器

--- src/main/java/com/duyanhan/forum/controller/CommonController.java
+++ src/main/java/com/duyanhan/forum/controller/CommonController.java
@@ -3,15 +3,25 @@ package com.duyanhan.forum.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 通用控制器
+ * @author duyanhan
+ *
+ */
+
 @Controller
-public class HomeController {
+public class CommonController {
+	
+	// 默认欢迎页
+	@RequestMapping(value="/")
+	public String index() {
+		return  "index";
+	}
+	
+	// 论坛主页
 	@RequestMapping(value="/home")
 	public String home() {
 		return "home";
 	}
 	
-	@RequestMapping(value="/")
-	public String index() {
-		return  "index";
-	}
 }
