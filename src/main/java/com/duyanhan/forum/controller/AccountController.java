195edee03688fd4fd0052fada658cbc0d5bb8dc2
duyanhan1995 <duyanhan1995@gmail.com>
2018-05-17 10:02:32 +0800

    修改数据库乱码问题

--- src/main/java/com/duyanhan/forum/controller/AccountController.java
+++ src/main/java/com/duyanhan/forum/controller/AccountController.java
@@ -1,50 +1,48 @@
 package com.duyanhan.forum.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 
 import com.duyanhan.forum.domain.UserInfo;
 import com.duyanhan.forum.service.UserService;
 
 @Controller
 @RequestMapping(value = "/user")
-@SessionAttributes("currentUser")
 public class AccountController {
-
 	private static final Log logger = LogFactory.getLog(AccountController.class);
+	
 	@Autowired
 	private UserService userService;
 
 	@RequestMapping(value = "/login")
 	public String login(
 			UserInfo userInfo,
+			HttpSession session,
 			Model model) {
 		// 获取登录返回结果
 		boolean loginResult = userService.login(userInfo);
-		System.out.println("1");
 		// 如果登录成功
 		if (loginResult) {
-			System.out.println("2");
-			logger.info("登录成功,没有放入model");
 			// 向前台传入当前用户名
-			model.addAttribute("currentUser", userInfo.getUsername());
+			session.setAttribute("currentUser", userInfo.getUsername());
+			logger.info("用户{"+userInfo.getUsername()+"}登录成功，用户名存入session");
 		}
 		else {
-			System.out.println("3");
-			logger.error("登录失败，查无此人");
+			logger.error("用户{"+userInfo.getUsername()+"}登录失败，数据库查无此人");
 		}
 		return "home";
 	}
 
 	@RequestMapping(value = "/logout")
-	public String logout(SessionStatus sessionStatus) {
-		sessionStatus.setComplete();
+	public String logout(HttpSession session) {
+		session.removeAttribute("currentUser");
+		logger.info("用户{"+session.getAttribute("currentUser")+"}注销成功");
 		return "home";
 	}
 }
