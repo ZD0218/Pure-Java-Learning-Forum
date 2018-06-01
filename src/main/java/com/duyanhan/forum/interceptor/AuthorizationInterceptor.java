1253ed0bed83f229cb5bfdc0b42dbcd6c4c22508
duyanhan1995 <duyanhan1995@gmail.com>
2018-06-02 05:46:30 +0800

    用户登录认证拦截器已经实现，有待添加不被拦截的列表

--- src/main/java/com/duyanhan/forum/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/duyanhan/forum/interceptor/AuthorizationInterceptor.java
@@ -3,6 +3,8 @@ package com.duyanhan.forum.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,60 +16,51 @@ import com.duyanhan.forum.entity.User;
  *
  */
 public class AuthorizationInterceptor implements HandlerInterceptor {
+	private static final Log logger = LogFactory.getLog(AuthorizationInterceptor.class);
 
 	// 定义哪些请求是不需要被拦截的
-	private static final String[] IGNORE_URI = {"/home", "/register", "/",
-			"/blockTitleList","/blockPageList"
-			};
+	private static final String[] IGNORE_URI = {"/home", "/block", "/login","/register", "/index", "/postList"};
 	
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		
 		// 先对请求的路径进行判断，看是否需要进行拦截
-		boolean flag = false;
 		String currentRequestURI = request.getRequestURI();
+		logger.info("当前请求路径：" + currentRequestURI);
 		for (String ignoreURI : IGNORE_URI) {
 			if (currentRequestURI.contains(ignoreURI)) {
-				flag = true;
-				break;
+				logger.info("当前请求路径：" + currentRequestURI + "，由于包含[本该被放行路径:]" + ignoreURI + "，因此直接放行！");
+				return true;
 			}
 		}
 		
-		// 根据flag是否拦截请求
-		if (!flag) {
-			// 获取session中的用户信息
-			User user = (User) request.getSession().getAttribute("currentUser");
-			// 判断用户是否登录
-			if (user == null) {
-				System.out.println(".....................................");
-				// 如果用户没有登录
-				System.out.println("拦截当前的请求：" + currentRequestURI);
-				System.out.println("请先登录,再执行此操作");
-				// 请求转发
-				request.getRequestDispatcher("loginForm").forward(request, response);
-			} else {
-				// 如果用户已经登录，则验证通过，放行
-				System.out.println("用户已经登录，放行请求：" + currentRequestURI);
-				flag = true;
-			}
+		User user = (User) request.getSession().getAttribute("currentUser");
+		System.out.println(user);
+		// 判断用户是否登录
+		if (user == null) {
+			System.out.println(".....................................");
+			// 如果用户没有登录
+			logger.info("拦截当前的用户请求：" + currentRequestURI);
+			logger.info("请先登录,再执行此操作，即将跳转至登录页面");
+			// 重定向至登录页面
+			response.sendRedirect(request.getContextPath() + "/loginForm");
+			return false;
 		}
-		
-		return flag;
+		// 如果用户已经登录，则验证通过，放行
+		logger.info("用户已经登录，放行请求：" + currentRequestURI);
+		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
